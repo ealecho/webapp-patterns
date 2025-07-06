5f0d40eb909f1c1d43835695c8fc05672823c87b
ealecho <elshore32@gmail.com>
2025-07-06 08:13:29 +0300

    new branch

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 globalThis.DOM = {};
 
+//easy access withing this file
 const DOM = globalThis.DOM;
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', ()=> {
+    //Create references we will need later
 
-    // Create reference to the DOM object we shall need later
     DOM.todoList = document.getElementById('todo-list');
     DOM.addBtn = document.getElementById('add-btn');
     DOM.todoInput = document.getElementById('todo-input');
 
-    // Event listeners
+    //Event listeners
     DOM.addBtn.addEventListener('click', () => {
-        // TODO
+        //TODO
     });
-
-    DOM.todoList.addEventListener('click', (event) => {
-        if (event.target.classList.contains('delete-btn')) {
-            // TODO
-        }
-    });
-
-});
+})
